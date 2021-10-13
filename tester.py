8993568e94da290054837e0e7e75029a0f0bdfe0
JayArghArgh <jayarghargh@gmail.com>
2021-10-13 12:41:12 +1000

    add basic examples to testers.py

--- tester.py
+++ tester.py
@@ -7,28 +7,38 @@ EASTING = surveyPoint.EASTING
 NORTHING = surveyPoint.NORTHING
 ELEVATION = surveyPoint.ELEVATION
 
+# Start a point store so you can track your points.
 my_point_store = pointStore.PointStore()
+
+# Import the common calculations.
 my_math = commonCalculations.CommonCalculations()
 
+# Create some points and add to point store.
 point_1 = surveyPoint.SurveyPoint("JRR")
-point_2 = surveyPoint.SurveyPoint("JayArghArgh")
-
 my_point_store.set_new_point(point_1)
+
+point_2 = surveyPoint.SurveyPoint("JayArghArgh")
 my_point_store.set_new_point(point_2)
 
+# Start playing
 point_1.set_vertex(
     {EASTING: 100}
 )
 
-point_1.set_vertex({NORTHING: 200.123, ELEVATION: 56.123})
-
-print(point_1.get_vertex())
+point_1.set_vertex(
+    {
+        NORTHING: 200.123,
+        ELEVATION: 56.123
+    }
+)
 
 current_points = my_point_store.get_point_store()
+for k, v in current_points.items():
+    print(current_points[k].get_point_name())
+    print(current_points[k].get_status())
+    print(current_points[k].get_created_dtg())
+    print(current_points[k].get_vertex())
 
-# for k, v in current_points.items():
-#     print(current_points[k].get_point_name())
-    # print(current_points[x].get_status())
-    # print(current_points[x].get_created_dtg())
-
-# print(my_math.get_bearing_distance_from_coordinates(point_1.get_vertex(), point_2.get_vertex()))
+# Return bearing, distance 2d, distance 3d.
+print(my_math.get_bearing_distance_from_coordinates(point_1.get_vertex(), point_2.get_vertex()))
+# TODO currently returning back-bearing.
